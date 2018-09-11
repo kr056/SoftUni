c1e3370b276c6fdf8ac9f6e318d1143b6ac7b401
Bogdan Rachev <br056@abv.bg>
2018-09-11 17:12:12 +0300

    added solutions

---
+++ Programming Fundamentals May 2017/01_C# Into and Basics Exercises/Beverage Labels/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            int ml = int.Parse(Console.ReadLine());
+            int energyContentPer100ml = int.Parse(Console.ReadLine());
+            int sugarContentPer100ml = int.Parse(Console.ReadLine());
+
+            double energy = (ml / 100.0) * energyContentPer100ml;
+            double sugar = (ml / 100.0) * sugarContentPer100ml;
+
+
+            Console.WriteLine($"{ml}ml {name}:");
+            Console.WriteLine($"{energy}kcal, {sugar}g sugars");
+
+        }
+    }
+}
---
+++ Programming Fundamentals May 2017/01_C# Into and Basics Exercises/Character/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            int healthCurrent = int.Parse(Console.ReadLine());
+            int healthMax = int.Parse(Console.ReadLine());
+            int energyCurrent = int.Parse(Console.ReadLine());
+            int energyMax = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.Write("Health: |");
+            Console.Write(drawing("|",healthCurrent));
+            Console.Write(drawing(".", (healthMax - healthCurrent)));
+            Console.WriteLine("|");
+
+            Console.Write("Energy: |");
+            Console.Write(drawing("|", energyCurrent));
+            Console.Write(drawing(".", (energyMax - energyCurrent)));
+            Console.WriteLine("|");
+
+
+
+
+
+
+
+        }
+        public static string drawing(string text, int repeatCount)
+        {
+            string a = "";
+            for (int i = 0; i < repeatCount; i++)
+            {
+                a += text;
+            }
+            return a;
+        }
+    }
+  
+
+}
