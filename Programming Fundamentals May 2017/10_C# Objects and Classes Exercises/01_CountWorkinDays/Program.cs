b15ce5929863f96e76c73f536f1aa4c1d0065e5f
brachev56 <br056@abv.bg>
2018-09-13 11:46:30 +0300

    added updated solutions

---
+++ Programming Fundamentals May 2017/10_C# Objects and Classes Exercises/01_CountWorkinDays/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_CountWorkinDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var holidays = new[]
+            {
+                "01 01",
+                "03 03",
+                "01 05",
+                "06 05",
+                "24 05",
+                "06 09",
+                "22 09",
+                "01 11",
+                "24 12",
+                "25 12",
+                "26 12",
+            }
+            .Select(a => DateTime.ParseExact(a, "dd MM", CultureInfo.InvariantCulture)).ToArray();
+
+            var startDate =
+                DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            var endDate =
+                DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            var workingDaysCount = 0;
+
+            for (DateTime currentDate = startDate; currentDate <= endDate; currentDate = currentDate.AddDays(1))
+            {
+                var isSatOrSun =
+                    currentDate.DayOfWeek == DayOfWeek.Saturday || currentDate.DayOfWeek == DayOfWeek.Sunday;
+
+                var isHoliday = holidays
+                    .Any(d => d.Day == currentDate.Day && d.Month == currentDate.Month);
+
+                var isWorkingDay = !(isSatOrSun || isHoliday);
+
+                if (isWorkingDay)
+                {
+                    workingDaysCount++;
+                }
+            }
+
+            Console.WriteLine(workingDaysCount);
+
+        }
+    }
+}
---
+++ Programming Fundamentals May 2017/10_C# Objects and Classes Exercises/03_IntersectionOfCircles/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_IntersectionOfCircles
+{
+    class Circle
+    {
+        public Point Center { get; set; }
+        public int Radius { get; set; }
+
+    }
+
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstCircle = ReadCircle();
+            var secondCircle = ReadCircle();
+
+            var distanceBeetweenCircleCenters = DistanceBeetweenCirclesCenters(firstCircle, secondCircle);
+            var circleRadiuses = firstCircle.Radius + secondCircle.Radius;
+
+            Console.WriteLine(distanceBeetweenCircleCenters > circleRadiuses ? "No" : "Yes");
+
+        }
+        static Circle ReadCircle()
+        {
+            var circleInfo = Console.ReadLine().Split()
+                .Select(int.Parse).ToArray();
+
+            Circle circle = new Circle
+            {
+
+                Center = new Point
+                {
+                    X = circleInfo[0],
+                    Y = circleInfo[1],
+                },
+                Radius = circleInfo[2]
+            };
+
+            return circle;
+        }
+
+        static double DistanceBeetweenCirclesCenters(Circle first, Circle second)
+        {
+            var distX = first.Center.X - second.Center.X;
+            var distY = first.Center.Y - second.Center.Y;
+
+            return Math.Sqrt(distX * distX + distY * distY);
+        }
+    }
+
+}
